02dd5db6f73649f777b3acdaa12c5d7897b0b80d
Sławomir Aleksak <34636@studentwsei.pl>
2021-11-20 15:15:05 +0100

    Dodaj pliki projektów.

---
+++ IPrzedzialy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    interface IPrzedzialy
+    {
+        int Id { get; }
+        string Name { get; }
+        decimal rangeFrom();
+        decimal rangeTo();
+    }
+}
